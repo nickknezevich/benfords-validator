225b25a0cc08386c26242f029d65a14a19937136
Nikola Knezevic <48396463+nickknezevich@users.noreply.github.com>
2023-08-03 09:33:09 -0700

    nginx updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {    // <-- this object is added
-    port: 8000
-  },
   plugins: [
     vue(),
   ],
